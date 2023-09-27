e73c96a4a5a4a2f00193b800575003ac61dbb7d7
gopisf4061 <121146627+gopisf4061@users.noreply.github.com>
2023-09-27 20:32:10 +0530

    ej2(847785) - New UG samples for image editor new features in TS platform

--- ej2-javascript/code-snippet/image-editor/frame-cs1/index.js
+++ ej2-javascript/code-snippet/image-editor/frame-cs1/index.js
@@ -10,5 +10,21 @@ var imageEditorObj = new ej.imageeditor.ImageEditor({
   });
   imageEditorObj.appendTo('#imageeditor');
 
+  document.getElementById('matClick').onclick = function() {
+    imageEditorObj.drawFrame(FrameType.Mat, 'red', 'blue', 20, 20, 20, 20, FrameLineStyle.Solid, 1);
+  }
+  document.getElementById('bevelClick').onclick = function() {
+    imageEditorObj.drawFrame(FrameType.Bevel, 'red', 'blue', 20, 20, 20, 20, FrameLineStyle.Solid, 1);
+  }
+  document.getElementById('lineClick').onclick = function() {
+    imageEditorObj.drawFrame(FrameType.Line, 'red', 'blue', 20, 20, 20, 20, FrameLineStyle.Solid, 1);
+  }
+  document.getElementById('insetClick').onclick = function() {
+    imageEditorObj.drawFrame(FrameType.Inset, 'red', 'blue', 20, 20, 20, 20, FrameLineStyle.Solid, 1);
+  }
+  document.getElementById('hookClick').onclick = function() {
+    imageEditorObj.drawFrame(FrameType.Hook, 'red', 'blue', 20, 20, 20, 20, FrameLineStyle.Solid, 1);
+  }
+
 
 
--- ej2-javascript/code-snippet/image-editor/frame-cs1/index.ts
+++ ej2-javascript/code-snippet/image-editor/frame-cs1/index.ts
@@ -18,4 +18,20 @@ import { Browser } from '@syncfusion/ej2-base';
 
      //Button click
 
+     document.getElementById('matClick').onclick = (): void => {
+        imageEditorObj.drawFrame(FrameType.Mat, 'red', 'blue', 20, 20, 20, 20, FrameLineStyle.Solid, 1);
+    }
+    document.getElementById('bevelClick').onclick = (): void => {
+        imageEditorObj.drawFrame(FrameType.Bevel, 'red', 'blue', 20, 20, 20, 20, FrameLineStyle.Solid, 1);
+    }
+    document.getElementById('lineClick').onclick = (): void => {
+        imageEditorObj.drawFrame(FrameType.Line, 'red', 'blue', 20, 20, 20, 20, FrameLineStyle.Solid, 1);
+    }
+    document.getElementById('insetClick').onclick = (): void => {
+        imageEditorObj.drawFrame(FrameType.Inset, 'red', 'blue', 20, 20, 20, 20, FrameLineStyle.Solid, 1);
+    }
+    document.getElementById('hookClick').onclick = (): void => {
+        imageEditorObj.drawFrame(FrameType.Hook, 'red', 'blue', 20, 20, 20, 20, FrameLineStyle.Solid, 1);
+    }
+
 
