30788a3f0ba93aa7721e92adab5d643099249361
Jaganaath24-Baskaran <jaganaath.sf3462@gmail.com>
2023-12-20 10:45:11 +0530

    861696: Modified straightening sample in js / ts platforms

--- ej2-javascript/code-snippet/image-editor/annotation-cs14/index.js
+++ ej2-javascript/code-snippet/image-editor/annotation-cs14/index.js
@@ -1,52 +1,12 @@
 ej.base.enableRipple(true);
 
-var isBtnClick = false;
 var imageEditorObj = new ej.imageeditor.ImageEditor({
     width: '550px',
     height: '330px',
-    toolbar: [],
     fontFamily: { default: 'Arial', items: [{id: 'arial', text: 'Arial'}, {id: 'brush script mt', text: 'Brush Script MT'},
       {id: 'papyrus', text: 'Papyrus'}, {id: 'times new roman', text: 'Times New Roman'}, {id: 'courier new', text: 'Courier New'}] },
     created: function () {
       imageEditorObj.open('bee-eater.png');
-    },
-    fileOpened: function () {
-      if (!isBtnClick) {
-        let dimension = imageEditorObj.getImageDimension();
-        imageEditorObj.drawText(dimension.x,dimension.y, null, 'Arial');
-      }
-    },
+    }
   });
   imageEditorObj.appendTo('#imageeditor');
-
-  document.getElementById('brushScriptMT').onclick = function() {
-    isBtnClick = true;
-    imageEditorObj.reset();
-    let dimension = imageEditorObj.getImageDimension();
-	  imageEditorObj.drawText(dimension.x,dimension.y, null, 'Brush Script MT');
-    isBtnClick = false;
-  }
-
-  document.getElementById('papyrus').onclick = function() {
-    isBtnClick = true;
-    imageEditorObj.reset();
-    let dimension = imageEditorObj.getImageDimension();
-	  imageEditorObj.drawText(dimension.x,dimension.y, null, 'Papyrus');
-    isBtnClick = false;
-  }
-
-  document.getElementById('timesNewRoman').onclick = function() {
-    isBtnClick = true;
-    imageEditorObj.reset();
-    let dimension = imageEditorObj.getImageDimension();
-	  imageEditorObj.drawText(dimension.x,dimension.y, null, 'Times New Roman');
-    isBtnClick = false;
-  }
-
-  document.getElementById('courierNew').onclick = function() {
-    isBtnClick = true;
-    imageEditorObj.reset();
-    let dimension = imageEditorObj.getImageDimension();
-	  imageEditorObj.drawText(dimension.x,dimension.y, null, 'Courier New');
-    isBtnClick = false;
-  }
--- ej2-javascript/code-snippet/image-editor/annotation-cs14/index.ts
+++ ej2-javascript/code-snippet/image-editor/annotation-cs14/index.ts
@@ -6,53 +6,13 @@ import { Browser } from '@syncfusion/ej2-base';
 
 //Image Editor items definition
 
- let isBtnClick: boolean = false;
  let imageEditorObj: ImageEditor = new ImageEditor({
     width: '550px',
     height: '330px',
-    toolbar: [],
     fontFamily: { default: 'Arial', items: [{id: 'arial', text: 'Arial'}, {id: 'brush script mt', text: 'Brush Script MT'},
       {id: 'papyrus', text: 'Papyrus'}, {id: 'times new roman', text: 'Times New Roman'}, {id: 'courier new', text: 'Courier New'}] },
     created: () => {
         imageEditorObj.open('bee-eater.png');
-    },
-    fileOpened: function () {
-        if (!isBtnClick) {
-          let dimension = imageEditorObj.getImageDimension();
-          imageEditorObj.drawText(dimension.x,dimension.y, null, 'Arial');
-        }
-    },
+    }
     });
-    imageEditorObj.appendTo('#imageeditor');
-
-    document.getElementById('brushScriptMT').onclick = (): void => {
-        isBtnClick = true;
-        imageEditorObj.reset();
-        let dimension = imageEditorObj.getImageDimension();
-        imageEditorObj.drawText(dimension.x,dimension.y, null, 'Brush Script MT');
-        isBtnClick = false;
-    }    
-
-    document.getElementById('papyrus').onclick = (): void => {
-        isBtnClick = true;
-        imageEditorObj.reset();
-        let dimension = imageEditorObj.getImageDimension();
-        imageEditorObj.drawText(dimension.x,dimension.y, null, 'Papyrus');
-        isBtnClick = false;
-    }
-
-    document.getElementById('timesNewRoman').onclick = (): void => {
-        isBtnClick = true;
-        imageEditorObj.reset();
-        let dimension = imageEditorObj.getImageDimension();
-        imageEditorObj.drawText(dimension.x,dimension.y, null, 'Times New Roman');
-        isBtnClick = false;
-    }
-
-    document.getElementById('courierNew').onclick = (): void => {
-        isBtnClick = true;
-        imageEditorObj.reset();
-        let dimension = imageEditorObj.getImageDimension();
-        imageEditorObj.drawText(dimension.x,dimension.y, null, 'Courier New');
-        isBtnClick = false;
-    }
+ imageEditorObj.appendTo('#imageeditor');
